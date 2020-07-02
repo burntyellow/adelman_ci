ed0497ab0dae9c79758a339b0e6ddb4832e33b9e
Kim F. Wong <kimwong@pitt.edu>
2020-07-02 12:51:17 -0400

    Setting up travis CI.

---
+++ test_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Testing WESTPA installation."
+
+echo "WEST_ROOT   = " $WEST_ROOT
+echo "WEST_BIN    = " $WEST_BIN
+echo "WEST_PYTHON = " $WEST_PYTHON
+
+. westpa.sh
+
+echo "WEST_ROOT   = " $WEST_ROOT
+echo "WEST_BIN    = " $WEST_BIN
+echo "WEST_PYTHON = " $WEST_PYTHON
+
+
